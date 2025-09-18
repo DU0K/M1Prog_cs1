13a5ba940ce8fc1eb4436f7079861490fb3d77e3
DU0K <svenbras09@gmail.com>
2025-09-18 10:07:39 +0200

    m1prog initial commit

---
+++ opdrachten/02_var/stringvar/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace stringvar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string messageA = "A little les conversation ";
+        string messageB = "A little more action please";
+        Console.WriteLine(messageA + messageB);
+    }
+}
---
+++ opdrachten/02_var/variableopdracht1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace variableopdracht1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int leeftijd = 100;
+        string naam = " Elvis ";
+        bool leeftnog = false;
+        float drijfding = 50f;
+        Console.WriteLine(leeftijd + naam + leeftnog + drijfding);
+    }
+}
