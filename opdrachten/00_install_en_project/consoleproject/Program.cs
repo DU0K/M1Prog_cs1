8cfe5b3e6e0fae5efcc059764c03e122f0801e02
DU0K <svenbras09@gmail.com>
2025-09-18 09:14:05 +0200

    m1prog initial commit

---
+++ opdrachten/00_install_en_project/consoleproject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace consoleproject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1 for the money 2 for the joe 3 to get ready now gogogo and dont you step on my blue suede shoes!");
+        Console.WriteLine("Hello, world");
+    }
+}
