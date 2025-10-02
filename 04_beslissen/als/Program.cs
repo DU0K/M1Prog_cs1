98ed5d749c9d0b47e867b684c786e9a5960e6aad
DU0K <svenbras09@gmail.com>
2025-10-02 12:16:58 +0200

    opdracht4.2

---
+++ 04_beslissen/als/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace als;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("je zoekt je sleutel");
+        bool sleuterwkijt = false;
+        if (sleuterwkijt == false)
+        {
+            Console.WriteLine("Je bent je sleutel kwijt!");
+            Console.WriteLine("de deur blijft gesloten");
+        }
+    }
+}
