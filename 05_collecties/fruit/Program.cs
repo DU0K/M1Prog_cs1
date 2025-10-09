8f3854ced8cf3faec812ea364c3789cdca51ffb6
DU0K <svenbras09@gmail.com>
2025-10-09 11:55:59 +0200

    commit

---
+++ 05_collecties/fruit/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace fruit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] fruit = new string[]
+        { "passievrucht", "kers" };
+        string[] groente = new string[2];
+
+        int[] geluksgettallen = new int[]
+        {4, 7, 8};
+        int[] tottien = new int[10];
+
+        bool[] bolloDeBeer = new bool[6];
+        float[] drijfding = new float[3];
+    }
+}
