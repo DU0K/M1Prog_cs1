ae66154aac18a0e384cc76ec61438dd11b3dec19
DU0K <svenbras09@gmail.com>
2025-10-09 12:25:03 +0200

    commit

---
+++ 05_collecties/uitvragen/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace uitvragen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] percentages = new double[] { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6 };
+        Console.WriteLine("eerste in array heeft waarde " + percentages[0]);
+        Console.WriteLine(percentages[0]);
+        Console.WriteLine(percentages[1]);
+        Console.WriteLine(percentages[2]);
+        Console.WriteLine(percentages[3]);
+        Console.WriteLine(percentages[4]);
+        Console.WriteLine(percentages[5]);
+    }
+}
