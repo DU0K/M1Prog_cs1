693517f448388aca896d6e882fdbf50457c6e6f3
DU0K <svenbras09@gmail.com>
2025-10-17 09:05:58 +0200

    commit

---
+++ 06_loops/01 loop/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _01_loop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nummers = new int[]{42, 60, 89};
+        for(int i =0; i< nummers.Length; i)
+        {
+            Console.WriteLine(nummers);
+        }
+    }
+}
