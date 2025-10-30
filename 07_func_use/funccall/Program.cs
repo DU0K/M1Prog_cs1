d7b642db4b1809f75ebad765a24b117db7832ae1
DU0K <svenbras09@gmail.com>
2025-10-30 12:59:07 +0100

    commit

---
+++ 07_func_use/funccall/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace funccall;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Clear();
+        Console.Write("....X....");
+        public static void SetCursorPosition(int left, int top);
+    }
+}
---
+++ 07_func_use/math/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace math;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        void min()
+        {
+            double kleiner = 9;
+            double groter = 19;
+            double welkeIsKleiner = Math.Min(kleiner, groter);
+            Console.WriteLine(welkeIsKleiner + " is de kleinste van " + kleiner + " en " + groter);
+        }
+        void max()
+        {
+            double kleiner = 9;
+            double groter = 14;
+            double welkeIsGroter = Math.Max(kleiner, groter);
+            Console.WriteLine(welkeIsGroter + " is de grootste van " + kleiner + " en " + groter);
+        }
+
+        min();
+        max();
+    }
+}
